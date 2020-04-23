f62846de05ee96098ebd49a93e0b5a75928962a0
weizheng1910 <weizheng1910@gmail.com>
2020-04-23 20:13:07 +0800

    -

--- config/initializers/cloudinary.rb
+++
@@ -1,7 +0,0 @@
-Cloudinary.config do |config|
-  config.cloud_name = process.env.CLOUDINARY_CLOUD_NAME
-  config.api_key = process.env.CLOUDINARY_API_SECRET
-  config.api_secret = process.env.CLOUDINARY_CLOUD_NAME
-  config.secure = true
-  config.cdn_subdomain = true
-end
